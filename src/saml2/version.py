34d595bc841e3d8589afc0b0b6eeeeca8546eebe
phlip <git@mrphlip.com>
2022-05-31 17:45:00 +1000

    Don't try to get version from pkg_resources
    Seems to not be playing nice with py2exe

--- src/saml2/version.py
+++ src/saml2/version.py
@@ -2,8 +2,7 @@ import pkg_resources as _pkg_resources
 
 
 def _parse_version():
-    data = _pkg_resources.get_distribution('pysaml2')
-    value = _pkg_resources.parse_version(data.version)
+    value = _pkg_resources.parse_version("4.9.0.post1+xswbackport")
     return value
 
 
